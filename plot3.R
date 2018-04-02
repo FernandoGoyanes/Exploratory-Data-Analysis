8cdd6e164a7cf4858d49e688993251a84c1f965a
Fernando Goyanes <fernando_goyanes@hotmail.co.uk>
2018-04-02 23:55:25 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,51 +1,21 @@
-## Getting full dataset
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
-
-
-## Subsetting the data
-
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-
-rm(data_full)
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-## Converting dates
-
-datetime <- paste(as.Date(data$Date), data$Time)
-
-data$Datetime <- as.POSIXct(datetime)
-
-
-
-## Plot 3
-
-with(data, {
-
-    plot(Sub_metering_1~Datetime, type="l",
-
-         ylab="Global Active Power (kilowatts)", xlab="")
-
-    lines(Sub_metering_2~Datetime,col='Red')
-
-    lines(Sub_metering_3~Datetime,col='Blue')
-
-})
-
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-
-
-## Saving to file
-
-dev.copy(png, file="plot3.png", height=480, width=480)
-
-dev.off()
+# annotating graph
+title(main="Energy sub-metering")
