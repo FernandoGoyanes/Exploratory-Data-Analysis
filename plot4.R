8093aee142f3580b0a9fc2dac570b15d4c996bed
Fernando Goyanes <fernando_goyanes@hotmail.co.uk>
2018-04-02 23:56:44 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,65 +1,26 @@
-## Getting full dataset
+# Reading, naming and subsetting power consumption data
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
-
-
-## Subsetting the data
-
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-
-rm(data_full)
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
 
+# initiating a composite plot with many graphs
+par(mfrow=c(2,2))
 
-## Converting dates
-
-datetime <- paste(as.Date(data$Date), data$Time)
-
-data$Datetime <- as.POSIXct(datetime)
-
-
-
-## Plot 4
-
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-
-with(data, {
-
-    plot(Global_active_power~Datetime, type="l", 
-
-         ylab="Global Active Power (kilowatts)", xlab="")
-
-    plot(Voltage~Datetime, type="l", 
-
-         ylab="Voltage (volt)", xlab="")
-
-    plot(Sub_metering_1~Datetime, type="l", 
-
-         ylab="Global Active Power (kilowatts)", xlab="")
-
-    lines(Sub_metering_2~Datetime,col='Red')
-
-    lines(Sub_metering_3~Datetime,col='Blue')
-
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-    plot(Global_reactive_power~Datetime, type="l", 
-
-         ylab="Global Rective Power (kilowatts)",xlab="")
-
+# calling the basic plot function that calls different plot functions to build the 4 plots that form the graph
+with(subpower,{
+  plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
+  plot(subpower$Time,as.numeric(as.character(subpower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
+  plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+   legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+  plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
 })
-
-
-
-## Saving to file
-
-dev.copy(png, file="plot4.png", height=480, width=480)
-
-dev.off()
